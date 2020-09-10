1dcdccd00bf363d87e62b7b01914c51bc8ad045a
Erlang <71025266+3RL4N9@users.noreply.github.com>
2020-09-10 22:55:02 +0700

    Add files via upload

---
+++ prank.py
@@ -0,0 +1,32 @@
+#ingat ini hanya untuk ngeprank temen
+#bahasa= python
+
+import os,time,random
+
+os.system('clear')
+time.sleep(2.00)
+os.system('toilet -f standard Hacking -F gay')
+
+print("""\33[35m
+|~~~~~~~~~~~~~~~~~~~~~~|
+|WhatsApp Hacking Tools|
+|~~~~~~~~~~~~~~~~~~~~~~|
+|{?}Example=08xxxxxxxxx|
+|~~~~~~~~~~~~~~~~~~~~~~|
+""")
+
+
+nmr = input('\33[4m{?}ketik nomor korban ~# ')
+time.sleep(2.00)
+kode = random.choice(["099154","387567","157586","102679","268909","457432","452891","170845","190605"])
+time.sleep(2.00)
+print('{√}Sedang Login')
+time.sleep(2.00)
+print("{√}Login Sukses")
+time.sleep(2.00)
+print("{√}Dump SMS")
+time.sleep(2.00)
+print("{√}Code Verifikasi Siap : "+kode)
+time.sleep(2.00)
+print("{•}thanks for using my tools{•}")
+
